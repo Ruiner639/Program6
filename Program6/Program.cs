0de4acc9d4bba5d2bd1273cd65981451cdda2189
Dmitriy <70809857+Ruiner639@users.noreply.github.com>
2020-12-07 15:58:42 +0300

    Добавьте файлы проекта.

---
+++ Program6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Program6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your height: ");
+            string height = Console.ReadLine();
+            Int32.TryParse(height, out int height_int);
+            if (height_int >= 190)
+            {
+                Console.WriteLine("You could be a good basketball player");
+            }
+            else if(height_int < 175)
+            {
+                Console.WriteLine("You could be a good horseman");
+            }
+            else
+            {
+                Console.WriteLine("You could be a good athlete");
+
+            }
+            
+        }
+    }
+}
